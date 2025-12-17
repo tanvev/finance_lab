3de062d31c4db7f07e013cadc2c7ad6954068143
tanvev <tanvisundarkar@gmail.com>
2025-12-17 07:19:24 +0530

    Change EMAIL_HOST_USER to sample email
    Updated email host user to a sample email address.

--- finance_lab/settings.py
+++ finance_lab/settings.py
@@ -84,7 +84,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "tanvi200705@gmail.com"
+EMAIL_HOST_USER = "sample@gmail.com"
 EMAIL_HOST_PASSWORD = ("Finance_lab")  # Use App Password (not your login!)
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
